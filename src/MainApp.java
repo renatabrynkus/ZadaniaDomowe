13a14e7cddfbfd3eafe918bfcdb135019f0f0e12
Renata <renata@gmail.com>
2021-01-23 18:08:21 +0100

    Added new Class Bug.

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,6 +1,15 @@
+import model.Bug;
+
 public class MainApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Bug bug1 = new Bug("It is not possible to log in", "johnny.snowmail@com", 10);
+
+        bug1.showAllData();
+
+        bug1.setBugDescription("Wrong");
+        bug1.setEmail("johnnytesetergmail.com");
+        bug1.setBugPriority(111);
+
     }
 }
