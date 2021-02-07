ce1c9091823d5725808b900fe2aba8725fe3444b
Renata <renata@gmail.com>
2021-02-07 17:39:32 +0100

    Added inheritance excercise.

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,6 +1,21 @@
+import computer.Laptop;
+import computer.PC;
+
 public class MainApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+
+
+        PC officeComputer = new PC("Office computer", "HP", 500, 128);
+        Laptop gamingLaptop = new Laptop("XGames", "HP GAMES", 500, 256, 50);
+
+        gamingLaptop.setBatteryLevel(0);
+        gamingLaptop.switchOn();
+        System.out.println(gamingLaptop.getState());
+
+        officeComputer.setPlugIn(false);
+        officeComputer.switchOn();
+        System.out.println(officeComputer.getState());
+
     }
 }
